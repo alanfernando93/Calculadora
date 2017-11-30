5abf9e7cd04c8b51494cf231b8ce099dd2d872d3
HUAYLLANI <HUAYLLANI@HUAYLLANI-PC>
2017-11-30 16:16:00 -0430

    Proyecto Cryptoanalisis 2 - Calculadora

--- public_html/js/mainBoot.js
+++ public_html/js/mainBoot.js
@@ -1,10 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 var aux = "", label2, r, array = [], num1, num2, signo = "", sw = 0;
 
 function init() {
